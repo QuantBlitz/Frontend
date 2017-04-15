e2d3f539c5351b5df96be816ddeef90ffa15cf7e
RecursiveLogic <luis@radiux.io>
2017-04-15 13:15:25 -0400

    Updated regex for username validation to validate extra characters

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -12,4 +12,4 @@ export const formatDate = date => {
   return `${date.getFullYear()}-${month}-${day}`
 }
 
-export const validUsername = username => username.match(/^[a-zA-Z0-9]+$/)
+export const validUsername = username => username.match(/^[a-zA-Z0-9\-_]+$/)
